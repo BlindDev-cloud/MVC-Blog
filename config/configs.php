12643fd7c109458cba93f26be735dfe6de19472e
BlindDev <korsikoff132@gmail.com>
2022-12-04 19:54:02 +0200

    [feature/router] Added constants + database configs through .env file to the project

---
+++ config/configs.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'db' => [
+        'host' => getenv('DB_HOST') ?? '',
+        'database' => getenv('DB_NAME') ?? '',
+        'user' => getenv('DB_USER') ?? '',
+        'password' => getenv('DB_PASSWORD') ?? ''
+    ]
+];
+
+if(!session_id()){
+    session_start();
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
-phpinfo();
+require_once dirname(__DIR__) . '/config/constants.php';
+require_once BASE_DIR . '/vendor/autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createUnsafeImmutable(BASE_DIR);
+$dotenv->load();
